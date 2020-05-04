58935c235088283422eea1b974af0da6f7a37ece
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2020-05-04 11:17:28 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,6 @@
+import time
+print("Welcome to the TestWiki:Inactivity Script")
+print("This script is only for the use of Consuls")
+print("Please now run the findInactive script")
+time.sleep(10)
+users = input("How many users are being removed?")
