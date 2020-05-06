f27dfb84925c25e9663026f422b067e56b030253
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2020-05-06 14:57:42 +0100

    revert
    going through CR/I

--- remove.py
+++ remove.py
@@ -13,11 +13,6 @@ while count < int(users):
   userlist.append(usertemp)
   count = count + 1
   time.sleep(0.5)
-fromheader = input("Your Email: ")
-headers = {
-    'User-Agent': 'PublicTestWikiInactiveAuto-github/rhinosf1-fortestwikiconusls',
-    'From': fromheader
-}
 S = requests.Session()
 URL = "https://test.miraheze.org/w/api.php"
 # Step 1: Retrieve a login token
@@ -27,12 +22,11 @@ PARAMS_1 = {
     "type": "login",
     "format": "json"
 }
-R = S.get(url=URL, params=PARAMS_1, headers=headers)
+R = S.get(url=URL, params=PARAMS_1)
 DATA = R.json()
 LOGIN_TOKEN = DATA["query"]["tokens"]["logintoken"]
 # Step 2: Send a post request to log in. See
 # https://www.mediawiki.org/wiki/Manual:Bot_passwords
-time.sleep(1) #wait 1s to avoid throttling
 username = input("Username: ")
 password = input("Password: ")
 PARAMS_2 = {
@@ -42,8 +36,7 @@ PARAMS_2 = {
     "lgtoken": LOGIN_TOKEN,
     "format": "json"
 }
-R = S.post(URL, data=PARAMS_2, headers=headers)
-time.sleep(1) #hold for 1s to avoid throttling
+R = S.post(URL, data=PARAMS_2)
 # Step 3: Obtain a Userrights token
 PARAMS_3 = {
     "action": "query",
@@ -51,7 +44,7 @@ PARAMS_3 = {
     "meta": "tokens",
     "type": "userrights"
 }
-R = S.get(url=URL, params=PARAMS_3, headers=headers)
+R = S.get(url=URL, params=PARAMS_3)
 DATA = R.json()
 
 USERRIGHTS_TOKEN = DATA["query"]["tokens"]["userrightstoken"]
@@ -59,7 +52,6 @@ USERRIGHTS_TOKEN = DATA["query"]["tokens"]["userrightstoken"]
 count = 0
 while count < len(userlist):
   inactiveuser = userlist[count]
-    time.sleep(5) #wait 5 seconds before write api
 # Step 4: Request to add or remove a user from a group
   PARAMS_4 = {
       "action": "userrights",
@@ -75,6 +67,7 @@ while count < len(userlist):
   DATA = R.json()
 
   print(DATA)
+  time.sleep(3)
 time.sleep(2)
 print('Generating mass message text..')
 print('{{subst:Inactivity|user='+username+'}}')
