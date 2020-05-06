6e9ace4091d9b71c6b4e18e1d3615ab6f11f6484
examknow <ek311@outlook.com>
2020-05-06 14:29:25 -0500

    adding notify

--- remove.py
+++ remove.py
@@ -83,6 +83,46 @@ def remove():
   time.sleep(5)
   print("Thanks for using! Good bye.")
   sys.exit()
+  
+def notify():
+  time.sleep(10)
+  consul = input("What is your username? ")
+  removedate = input("Removal date: ")
+  date = input("Today's Date: " )
+  users = input("How many users are being removed? ")
+  userlist = []
+  count = 0
+  while count < int(users):
+    usertemp = input("User to remove: ")
+    userlist.append(usertemp)
+    count = count + 1
+    time.sleep(0.5)
+  print("Generating mass message list....")
+  time.sleep(2)
+  count = 0
+  while count < len(userlist):
+    print("User Talk:" + str(userlist[count]))
+    count = count + 1
+    time.sleep(0.5)
+  print("Generating mass message text....")
+  time.sleep(2)
+  print("{{subst:InactiveReminder|DATE=" + removedate + "|sig=~~~ for [[User:"+consul +"|"+consul + "]]}}")
+  print("Generating Community Noticeboard post")
+  time.sleep(2)
+  print("==Inactive Rights Removal - "+ date + "==")
+  print("The rights of the following users will be removed on or after " + removedate + " if they do not return to activity:")
+  count = 0
+  while count < len(userlist):
+        print("*{{RFP/User|"+userlist[count]+"}}")
+        count = count + 1
+  print("")      
+  print("Thanks,")
+  print(":~~~")
+  print(":For the Consul Team")
+  print(":~~~~~")
+  time.sleep(5)
+  print("Thanks for using! Good bye.")
+
 
 
 
@@ -93,6 +133,11 @@ try:
     print("This script may only be used by consuls")
     print("Please ensure notifications were sent > 7 days ago and the users are still inacitve")
     remove()
+  if sys.argv[1] == 'notify':
+    print("Running Script in Notify Mode")
+    print("Before we begin, please run the findInactive script on https://publictestwiki.com")
+    print("The notification process will begin in 10 seconds")
+    notify()
   if sys.argv[1] == 'help':
     print("Commands are:")
     print("remove     Removes rights from inactive users")
