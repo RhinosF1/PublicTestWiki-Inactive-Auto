f2bfd591a0a53344366d7cc2cfb4b85d5c9500bc
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2020-05-06 11:42:16 +0100

    Update remove.py

--- remove.py
+++ remove.py
@@ -6,5 +6,5 @@ time.sleep(2)
 print("https://publictestwiki.com/wiki/Special:ApiSandbox#action=userrights&format=json&user=&add=&remove=bot%7Csysop%7Cbureaucrat%7Cconsul%7Ctestgroup%7Cautopatrolled%7Cconfirmed%7Crollbacker%7Cinterface-admin%7Cflow-bot%7Ccheckuser%7Cinterwiki-admin%7Coversight%7Csteward&reason=per%20%5B%5BTestWiki%3AInactivity%7CInactivity%20report%5D%5D&token=")
 time.sleep(2)
 userremoving = input("What is your username?")
-#Add Template:Inactivty when params fixed
+print('{{subst:Inactivity|user='+userremoving)
 time.sleep(2)
