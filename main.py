52256687b16a45e587a770bce347e4edd76a4f3b
examknow <ek311@outlook.com>
2020-05-07 08:53:17 -0500

    revert last

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def remove():
   # Step 2: Send a post request to log in. See
   # https://www.mediawiki.org/wiki/Manual:Bot_passwords
   time.sleep(1) #wait 1s to avoid throttling
-  username = input("Enter your wiki Username: ")
+  username = input("Username: ")
   password = stdiomask.getpass()
   PARAMS_2 = {
       "action": "login",
